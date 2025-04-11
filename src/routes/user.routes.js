42f9904195c894da8830615d8fa5db0bb18cb757
Tanvi Bansal <bansaltanvi2004@gmail.com>
2025-04-12 03:19:15 +0530

    Add New Routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,26 +1,55 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = Router() // just like how we make express ki app
+const router = Router(); // just like how we make express ki app
 
-router.route("/register").post(upload.fields([
+router.route("/register").post(
+  upload.fields([
     {
-        name: "avatar",
-        maxCount: 1
+      name: "avatar",
+      maxCount: 1,
     },
     {
-        name: "coverImage",
-        maxCount: 1
-    }
+      name: "coverImage",
+      maxCount: 1,
+    },
     // 2 objects because we are handling/uploading 2 files.
-]), registerUser)
+  ]),
+  registerUser
+);
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 // secured routes
-router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/change-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
-export default router
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile); // why this kinda route??
+
+router.route("/history").get(verifyJWT, getWatchHistory);
+
+export default router;
